e2df1db364f0cf1536cb27bbbfc22a2d4fd2edc6
mic-k-r <69399528+mic-k-r@users.noreply.github.com>
2021-05-08 13:46:31 -0500

    created mock recipe data without Ingredient class

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -14,7 +14,13 @@ export class RecipeListComponent implements OnInit {
       this.recipeList = [
         new Recipe("Red Snapper with Italian Salsa Verde", 
                   "1 lb red snapper, 2 onions, 1 bunch parsley", 
-                  "1. Cook red snapper in a cast iron pan, 2. Add the salsa verde.")
+                  "1. Cook red snapper in a cast iron pan, 2. Add the salsa verde."), 
+        new Recipe("Punjabi Aloo Beans",
+                  "1 lb French green beans, 3 lbs potatoes",
+                  "Put everything in an Instant Pot and pressure cook it."),
+        new Recipe("Shrimp Scampi",
+                  "1 lb raw shrimp, 1 lb linguini, 1 bunch parsley, 4 lemons",
+                  "1. Sautee shrimp")
       ];
    }
 
