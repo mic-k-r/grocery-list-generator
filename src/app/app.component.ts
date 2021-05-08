e012804b51a3fe210de6d8b373aaa84316f1e729
mic-k-r <69399528+mic-k-r@users.noreply.github.com>
2021-05-08 14:11:44 -0500

    moved mock recipe data to app.component.ts and updated app html and recipe-list ts and html files to display recipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './recipe';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Easy Meal Planning';
+
+  recipeList: Recipe[]; 
+
+  constructor() { 
+      this.recipeList = [
+        new Recipe("Red Snapper with Italian Salsa Verde", 
+                  "1 lb red snapper, 2 onions, 1 bunch parsley", 
+                  "1. Cook red snapper in a cast iron pan, 2. Add the salsa verde."), 
+        new Recipe("Punjabi Aloo Beans",
+                  "1 lb French green beans, 3 lbs potatoes",
+                  "Put everything in an Instant Pot and pressure cook it."),
+        new Recipe("Shrimp Scampi",
+                  "1 lb raw shrimp, 1 lb linguini, 1 bunch parsley, 4 lemons",
+                  "1. Sautee shrimp")
+      ];
+   }
 }
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe';
 
 @Component({
@@ -8,21 +8,9 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipeList: Recipe[]; 
+  @Input() recipes: Recipe[];
 
-  constructor() { 
-      this.recipeList = [
-        new Recipe("Red Snapper with Italian Salsa Verde", 
-                  "1 lb red snapper, 2 onions, 1 bunch parsley", 
-                  "1. Cook red snapper in a cast iron pan, 2. Add the salsa verde."), 
-        new Recipe("Punjabi Aloo Beans",
-                  "1 lb French green beans, 3 lbs potatoes",
-                  "Put everything in an Instant Pot and pressure cook it."),
-        new Recipe("Shrimp Scampi",
-                  "1 lb raw shrimp, 1 lb linguini, 1 bunch parsley, 4 lemons",
-                  "1. Sautee shrimp")
-      ];
-   }
+  constructor() {}
 
   ngOnInit() {
   }
